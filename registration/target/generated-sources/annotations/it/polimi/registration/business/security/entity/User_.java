8782499cce0bf0aa522006e1c96cc2b6e2c3fa0c
nemtajo <nemtajo@yahoo.com>
2014-12-22 19:37:50 +0100

    pocetni projekat dodat na nas git

---
+++ registration/target/generated-sources/annotations/it/polimi/registration/business/security/entity/User_.java
@@ -0,0 +1,16 @@
+package it.polimi.registration.business.security.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-22T18:43:58")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> groupName;
+    public static volatile SingularAttribute<User, String> name;
+    public static volatile SingularAttribute<User, String> email;
+
+}
